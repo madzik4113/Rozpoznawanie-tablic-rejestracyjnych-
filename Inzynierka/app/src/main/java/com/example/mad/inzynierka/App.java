b5ea3bc7f27258f0616e9edebc03e6f091973981
madzik4113 <madzik4113@gmail.com>
2015-10-31 21:23:39 +0100

    opencv && camera initialization

--- Inzynierka/app/src/main/java/com/example/mad/inzynierka/App.java
+++ Inzynierka/app/src/main/java/com/example/mad/inzynierka/App.java
@@ -1,10 +1,13 @@
 package com.example.mad.inzynierka;
 
 import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
+
+import org.opencv.android.OpenCVLoader;
 
 
 public class App extends Activity {
@@ -19,22 +22,33 @@ public class App extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app);
 
- //camera button action and other fuctions
+ //camera button action and opencv library tes
         cameraBtn = (ImageButton) findViewById(R.id.cameraBtn);
         cameraBtn.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "openCV etc", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Tutaj bedzie caly algorytm!", Toast.LENGTH_SHORT).show();
             }
         }));
  //setting button action
         setBtn = (ImageButton) findViewById(R.id.setBtn);
         setBtn.setOnClickListener(new View.OnClickListener() {
             @Override
+
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Settings", Toast.LENGTH_SHORT).show();
+                if(!OpenCVLoader.initDebug()) {
+                    Toast.makeText(getApplicationContext(), "OpenCV fail!", Toast.LENGTH_SHORT).show();
+                }
+                else if(!isDeviceSupportCamera()){
+                    Toast.makeText(getApplicationContext(), "Nie posiadasz kamery!", Toast.LENGTH_SHORT).show();
+                }
+                else if(OpenCVLoader.initDebug() && isDeviceSupportCamera()){
+                    Toast.makeText(getApplicationContext(), "Ustawienia sprawdzone. Mozesz korzystac z aplikacji swobodnie!", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+
+
 //album button action
         albumBtn = (ImageButton) findViewById(R.id.albumBtn);
         albumBtn.setOnClickListener(new View.OnClickListener() {
@@ -54,4 +68,16 @@ public class App extends Activity {
         });
     }
 
+//funckja sprawdzajaca czy sprzet ma kamere
+
+    private boolean isDeviceSupportCamera() {
+        if (getApplicationContext().getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_CAMERA)) {
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
 }
